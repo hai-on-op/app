a81d04a48331aa373711ee62f321842484450d79
Ardy <ardy@defi.sucks>
2023-12-20 11:22:16 -0400

    fix: auctions batch size

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -68,5 +68,5 @@ export const floatsTypes = {
 // Auctions
 export const NUMBER_OF_AUCTIONS_TO_SHOW = 15
 export const SURPLUS_BATCH_SIZE = 5_000_000 // blocks
-export const DEBT_BATCH_SIZE = 200_000 // blocks
-export const COLLATERAL_BATCH_SIZE = 200_000 // blocks
+export const DEBT_BATCH_SIZE = 5_000_000 // blocks
+export const COLLATERAL_BATCH_SIZE = 5_000_000 // blocks
