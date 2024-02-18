741e721e9020a4d180c5790844159558aa099e69
Ryan Hafner <r.hafner20@gmail.com>
2024-02-18 10:35:17 -0500

    rework dropdown menu in manage vault section

--- src/containers/Vaults/Manage/ManageDropdown.tsx
+++ src/containers/Vaults/Manage/ManageDropdown.tsx
@@ -1,85 +1,99 @@
 import { useMemo } from 'react'
 
-import { useStoreActions, useStoreState } from '~/store'
+import { IVault, ReactChildren, TokenKey } from '~/types'
+import { useStoreState } from '~/store'
 import { useVault } from '~/providers/VaultProvider'
 
-import { CenteredFlex, Text } from '~/styles'
+import styled from 'styled-components'
+import { CenteredFlex, Flex, HaiButton, Text } from '~/styles'
 import { BrandedDropdown, DropdownOption } from '~/components/BrandedDropdown'
 import { TokenArray } from '~/components/TokenArray'
+import { Plus } from 'react-feather'
 
 export function ManageDropdown() {
     const {
         connectWalletModel: { tokensData },
-        vaultModel: { list, vaultData },
+        vaultModel: { list },
     } = useStoreState((state) => state)
-    const { vaultModel: vaultActions } = useStoreActions((actions) => actions)
 
     const { vault, setActiveVault, updateForm, collateral } = useVault()
 
-    const symbols = Object.values(tokensData || {})
-        .filter(({ isCollateral }) => isCollateral)
-        .map(({ symbol }) => symbol)
-
     const { label, options } = useMemo(() => {
-        if (!vault)
-            return {
-                label: (
-                    <CenteredFlex $gap={8}>
-                        <Text>{collateral.name}</Text>
-                        <Text>•</Text>
-                        <Text>
-                            {symbols.indexOf(collateral.name) + 1} of {symbols.length}
-                        </Text>
-                    </CenteredFlex>
-                ),
-                options: symbols.map((symbol) => (
-                    <DropdownOption
-                        key={symbol}
-                        $active={symbol === collateral.name}
-                        onClick={() => {
-                            updateForm('clear')
-                            const url = new URL(window.location.href)
-                            url.searchParams.set('collateral', symbol)
-                            window.history.replaceState({}, '', url)
-                            vaultActions.setVaultData({
-                                ...vaultData,
-                                collateral: symbol,
-                            })
-                        }}
-                        style={{ paddingLeft: '8px' }}
-                    >
-                        <TokenArray tokens={[symbol as any]} hideLabel />
-                        <Text>{symbol}</Text>
-                    </DropdownOption>
-                )),
-            }
+        const symbols = Object.values(tokensData || {})
+            .filter(({ isCollateral }) => isCollateral)
+            .map(({ symbol }) => symbol)
+
+        const sortedVaults = symbols.reduce(
+            (obj, symbol) => {
+                if (!obj[symbol]) obj[symbol] = []
+                return obj
+            },
+            {} as Record<string, IVault[]>
+        )
+        list.forEach((listVault) => {
+            if (!sortedVaults[listVault.collateralName]) sortedVaults[listVault.collateralName] = [listVault]
+            else sortedVaults[listVault.collateralName].push(listVault)
+        })
         return {
             label: (
                 <CenteredFlex $gap={8}>
                     <Text>{collateral.name}</Text>
-                    <Text $fontWeight={400}>#{vault.id}</Text>
+                    {!!vault && <Text $fontWeight={400}>#{vault.id}</Text>}
                     <Text>•</Text>
-                    <Text>
-                        {list.findIndex(({ id }) => id === vault.id) + 1} of {list.length}
-                    </Text>
+                    {vault ? (
+                        <Text>
+                            {list.findIndex(({ id }) => id === vault.id) + 1} of {list.length}
+                        </Text>
+                    ) : (
+                        <Text>Open New</Text>
+                    )}
                 </CenteredFlex>
             ),
-            options: list.map((listVault) => (
-                <DropdownOption
-                    key={`${listVault.collateralName}-${listVault.id}`}
-                    $active={listVault.id === vault.id}
-                    onClick={() => setActiveVault({ vault })}
-                    style={{ paddingLeft: '10px' }}
-                >
-                    <TokenArray tokens={[listVault.collateralName as any]} hideLabel />
-                    <CenteredFlex $gap={8}>
-                        <Text>{collateral.name}</Text>
-                        <Text $fontWeight={400}>#{listVault.id}</Text>
-                    </CenteredFlex>
-                </DropdownOption>
-            )),
+            options: Object.entries(sortedVaults).reduce((options, [symbol, vaults]) => {
+                options.push(
+                    <Flex $width="100%" $justify="space-between" $align="center" $gap={8}>
+                        <CenteredFlex $gap={8}>
+                            <TokenArray tokens={[symbol as TokenKey]} hideLabel size={28} />
+                            <Text>{symbol}</Text>
+                        </CenteredFlex>
+                        <IconButton
+                            title={`Open New ${symbol} Vault`}
+                            onClick={() => {
+                                setActiveVault({
+                                    create: true,
+                                    vault: undefined,
+                                    collateralName: symbol,
+                                })
+                            }}
+                        >
+                            <Plus size={16} />
+                        </IconButton>
+                    </Flex>
+                )
+                if (!vaults.length) {
+                    options.push(<Separator />)
+                    return options
+                }
+                const temp = options.concat(
+                    vaults.map((listVault) => (
+                        <DropdownOption
+                            key={`${listVault.collateralName}-${listVault.id}`}
+                            $active={listVault.id === vault?.id}
+                            onClick={() => setActiveVault({ vault: listVault })}
+                        >
+                            {/* <TokenArray tokens={[listVault.collateralName as any]} hideLabel /> */}
+                            <CenteredFlex $gap={8}>
+                                <Text>Vault</Text>
+                                <Text $fontWeight={400}>#{listVault.id}</Text>
+                            </CenteredFlex>
+                        </DropdownOption>
+                    ))
+                )
+                temp.push(<Separator />)
+                return temp
+            }, [] as ReactChildren[]),
         }
-    }, [vault, setActiveVault, updateForm, collateral, symbols, vaultActions])
+    }, [vault, setActiveVault, updateForm, collateral, tokensData])
 
     if (vault && !list.length) return null
 
@@ -94,18 +108,22 @@ export function ManageDropdown() {
             }
         >
             {options}
-            {!!vault && (
-                <DropdownOption
-                    onClick={() =>
-                        setActiveVault({
-                            create: true,
-                            collateralName: vault.collateralName,
-                        })
-                    }
-                >
-                    Open New {vault.collateralName} Vault
-                </DropdownOption>
-            )}
         </BrandedDropdown>
     )
 }
+
+const IconButton = styled(HaiButton)`
+    width: 28px;
+    height: 28px;
+    padding: 0px;
+    justify-content: center;
+    align-items: center;
+`
+
+const Separator = styled.div`
+    width: 100%;
+    height: 2px;
+    background: rgba(0, 0, 0, 0.1);
+    flex-grow: 0;
+    flex-shrink: 0;
+`
