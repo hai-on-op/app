c9c3bf763b8c09c28e96f00db4dcac5eed5484a3
Ryan Hafner <r.hafner20@gmail.com>
2024-02-19 21:49:31 -0500

    rework getAuctionStatus to handle COMPLETED collateral auctions

--- src/utils/auctions/utils.ts
+++ src/utils/auctions/utils.ts
@@ -3,9 +3,11 @@ import { Status } from '../constants'
 
 type AuctionStatusProps = Pick<IAuction, 'auctionDeadline' | 'winner' | 'isClaimed' | 'englishAuctionType'>
 export function getAuctionStatus(auction: AuctionStatusProps) {
+    if (auction.isClaimed) return Status.COMPLETED
+    if (auction.winner) {
+        return auction.englishAuctionType === 'COLLATERAL' ? Status.COMPLETED : Status.SETTLING
+    }
+
     if (1000 * parseInt(auction.auctionDeadline) > Date.now()) return Status.LIVE
-    if (auction.isClaimed && auction.winner) return Status.COMPLETED
-    if (!auction.isClaimed && !auction.winner)
-        return auction.englishAuctionType === 'COLLATERAL' ? Status.LIVE : Status.RESTARTING
-    return Status.SETTLING
+    return auction.englishAuctionType === 'COLLATERAL' ? Status.LIVE : Status.RESTARTING
 }
