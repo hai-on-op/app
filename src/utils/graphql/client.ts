f5969053cafed473bc5a6652a0197d7a6cbbe851
Luke Hamilton <pies.eth@pm.me>
2024-02-19 19:29:25 -0500

    chore: update subgraph to live reference

--- src/utils/graphql/client.ts
+++ src/utils/graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-    uri: 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/hai-sepolia-redeploy/version/v1/api',
+    uri: 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/hai-sepolia-redeploy/api',
     cache: new InMemoryCache(),
 })
