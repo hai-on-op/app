8031ae3705e3ba2b5ff1613fcbe7c2b82f19dd85
Luke Hamilton <pies.eth@pm.me>
2024-02-19 19:10:32 -0500

    chore: update sdk and subgrapg for sepolia redeploy

--- src/utils/graphql/client.ts
+++ src/utils/graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-    uri: 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/hai-sepolia/version/v1.01/api',
+    uri: 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/hai-sepolia-redeploy/version/v1/api',
     cache: new InMemoryCache(),
 })
