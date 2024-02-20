06c7fb7712b07cedff03139ca7062d8bc1401fd7
Ryan Hafner <r.hafner20@gmail.com>
2024-02-20 09:33:57 -0500

    prettier

--- src/utils/auctions/utils.ts
+++ src/utils/auctions/utils.ts
@@ -3,7 +3,8 @@ import { Status } from '../constants'
 
 type AuctionStatusProps = Pick<IAuction, 'auctionDeadline' | 'winner' | 'isClaimed' | 'englishAuctionType'>
 export function getAuctionStatus(auction: AuctionStatusProps) {
-    if (1000 * parseInt(auction.auctionDeadline) > Date.now() && auction.englishAuctionType !== 'COLLATERAL') return Status.LIVE
+    if (1000 * parseInt(auction.auctionDeadline) > Date.now() && auction.englishAuctionType !== 'COLLATERAL')
+        return Status.LIVE
     if (auction.isClaimed) return Status.COMPLETED
     if (auction.winner) {
         return auction.englishAuctionType === 'COLLATERAL' ? Status.COMPLETED : Status.SETTLING
