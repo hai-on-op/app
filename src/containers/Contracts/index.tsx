a936b1b2f0a1f4cb90510b79ca598ee6cc7ee19d
Ryan Hafner <r.hafner20@gmail.com>
2024-03-11 14:33:38 -0400

    add external contract table

--- src/containers/Contracts/index.tsx
+++ src/containers/Contracts/index.tsx
@@ -10,6 +10,39 @@ import { BrandedTitle } from '~/components/BrandedTitle'
 import { AddressLink } from '~/components/AddressLink'
 import { Table, TableContainer } from '~/components/Table'
 import { Copyable } from '~/components/Copyable'
+import { Link } from '~/components/Link'
+import { ArrowUpRight } from 'react-feather'
+
+const externalContracts = [
+    {
+        address: '0x146b020399769339509c98b7b353d19130c150ec',
+        name: 'Uniswap V3 Pool - HAI/WETH (0.3%)',
+        description: `Uniswap liquidity pool. Incentivized with KITE and OP rewards (full-range only).`,
+        labelLink: 'View on Uniswap',
+        link: 'https://info.uniswap.org/#/optimism/pools/0x146b020399769339509c98b7b353d19130c150ec',
+    },
+    {
+        address: '0x2A087fd694DeBec1ED61E0740BD0810b804da8f0',
+        name: 'Uniswap V3 Pool - HAI/WETH (1%)',
+        description: `Uniswap liquidity pool.`,
+        labelLink: 'View on Uniswap',
+        link: 'https://info.uniswap.org/#/optimism/pools/0x2a087fd694debec1ed61e0740bd0810b804da8f0',
+    },
+    {
+        address: '0xbdED651C03E2bC332AA49C1ffCa391eAA3ea6B86',
+        name: 'Velodrome Pool - HAI/sUSD',
+        description: `Velodrome liquidity pool. Incentivized with KITE and OP rewards.`,
+        labelLink: 'View on Velodrome',
+        link: 'https://velodrome.finance/deposit?token0=0x10398AbC267496E49106B07dd6BE13364D10dC71&token1=0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9&type=0',
+    },
+    {
+        address: '0xf4638dC488F9C826DC40250515592E678E447238',
+        name: 'Velodrome Pool - KITE/OP',
+        description: `Velodrome liquidity pool. Incentivized with KITE rewards.`,
+        labelLink: 'View on Velodrome',
+        link: 'https://velodrome.finance/deposit?token0=0x4200000000000000000000000000000000000042&token1=0xf467C7d5a4A9C4687fFc7986aC6aD5A4c81E1404&type=-1',
+    },
+]
 
 const sortableHeaders: SortableHeader[] = [{ label: 'Contract' }, { label: 'Address' }, { label: 'Description' }].map(
     (obj) => ({ ...obj, unsortable: true })
@@ -52,27 +85,56 @@ export function Contracts() {
     const isUpToSmall = useMediaQuery('upToSmall')
 
     return (
-        <Container>
-            <Header>
-                <BrandedTitle textContent="CONTRACTS" $fontSize={isUpToSmall ? '2.4rem' : '3rem'} />
-            </Header>
-            <Table
-                container={StyledTableContainer}
-                headers={sortableHeaders}
-                headerContainer={TableHeader}
-                sorting={{ key: '', dir: 'desc' }}
-                setSorting={() => {}}
-                rows={contracts.map(({ name, address, description }) => (
-                    <TableRow key={name}>
-                        <Text $fontWeight={isUpToSmall ? 700 : 400}>{name}</Text>
-                        <Copyable text={address} limitClickToIcon>
-                            <AddressLink address={address} />
-                        </Copyable>
-                        <Text>{description}</Text>
-                    </TableRow>
-                ))}
-            />
-        </Container>
+        <>
+            <Container>
+                <Header>
+                    <BrandedTitle textContent="CONTRACTS" $fontSize={isUpToSmall ? '2.4rem' : '3rem'} />
+                </Header>
+                <Table
+                    container={StyledTableContainer}
+                    headers={sortableHeaders}
+                    headerContainer={TableHeader}
+                    sorting={{ key: '', dir: 'desc' }}
+                    setSorting={() => {}}
+                    rows={contracts.map(({ name, address, description }) => (
+                        <TableRow key={name}>
+                            <Text $fontWeight={isUpToSmall ? 700 : 400}>{name}</Text>
+                            <Copyable text={address} limitClickToIcon>
+                                <AddressLink address={address} />
+                            </Copyable>
+                            <Text>{description}</Text>
+                        </TableRow>
+                    ))}
+                />
+            </Container>
+            <Container>
+                <Header>
+                    <BrandedTitle textContent="EXTERNAL CONTRACTS" $fontSize={isUpToSmall ? '2.4rem' : '3rem'} />
+                </Header>
+                <Table
+                    container={StyledTableContainer}
+                    headers={sortableHeaders}
+                    headerContainer={TableHeader}
+                    sorting={{ key: '', dir: 'desc' }}
+                    setSorting={() => {}}
+                    rows={externalContracts.map(({ name, address, description, labelLink, link }) => (
+                        <TableRow key={name}>
+                            <Text $fontWeight={isUpToSmall ? 700 : 400}>{name}</Text>
+                            <Copyable text={address} limitClickToIcon>
+                                <AddressLink address={address} />
+                            </Copyable>
+                            <Text>
+                                {description}&nbsp;
+                                <Link href={link}>
+                                    <Text>{labelLink}</Text>
+                                    <ArrowUpRight size={16} />
+                                </Link>
+                            </Text>
+                        </TableRow>
+                    ))}
+                />
+            </Container>
+        </>
     )
 }
 
