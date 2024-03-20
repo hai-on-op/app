7e530b89e85d0587528ccae3cae3390abfdc3b0a
Alex Michelsen <57596674+icemanparachute@users.noreply.github.com>
2024-03-20 14:08:47 -0400

    Update endpoint.ts

--- _api/endpoint.ts
+++ _api/endpoint.ts
@@ -5,8 +5,17 @@ import ERC20_ABI from '../src/abis/erc20.json'
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const provider = new ethers.providers.JsonRpcProvider(process.env.VITE_MAINNET_PUBLIC_RPC)
     const address = '0xf467C7d5a4A9C4687fFc7986aC6aD5A4c81E1404'
+    const vestingAddress = '0x1bb64AF7FE05fc69c740609267d2AbE3e119Ef82'
     const erc20 = new ethers.Contract(address, ERC20_ABI, provider)
     const totalSupply = ethers.utils.formatUnits(await erc20.totalSupply())
+    const vesting = new ethers.Contract(vestingAddress, VESTING_ABI, provider);
+    let totalLockedTokens = 0;
+    for (let i = 1; i < 17; i++) {
+        let plan = await vesting.plans(i);
+        if (plan.token.toLowerCase() == address.toLowerCase()) {
+            totalLockedTokens += plan.amount;
+        }
+    }
     response.setHeader('Content-Type', 'text/plain')
     return response.send(totalSupply)
 }
