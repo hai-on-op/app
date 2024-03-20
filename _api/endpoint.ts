2e664c775ceedc0a76aa1d1f9a8743bd2c074557
Luke Hamilton <pies.eth@pm.me>
2024-03-20 00:47:27 -0400

    feat: test web3 call

--- _api/endpoint.ts
+++ _api/endpoint.ts
@@ -1,5 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { ethers } from 'ethers'
+import ERC20_ABI from '../src/abis/erc20.json'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-    return response.send('OK')
+    const provider = new ethers.providers.AlchemyProvider('optimism', process.env.VITE_ALCHEMY_KEY)
+    const address = '0xf467C7d5a4A9C4687fFc7986aC6aD5A4c81E1404'
+    const erc20 = new ethers.Contract(address, ERC20_ABI, provider)
+    const totalSupply = ethers.utils.formatUnits(await erc20.totalSupply())
+    return response.send(totalSupply)
 }
