d406700aab3ea04b773afa73618a6e0ae5ba3502
Luke Hamilton <pies.eth@pm.me>
2024-03-19 23:44:36 -0400

    feat: test edge function

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vercel from 'vite-plugin-vercel'
 import tsConfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
+
 export default defineConfig(() => ({
     resolve: {
         dedupe: ['buffer', 'bn.js', 'keccak', 'ethers'],
     },
-    plugins: [react(), tsConfigPaths()],
+    plugins: [react(), vercel(), tsConfigPaths()],
+    vercel: {
+        expiration: 25,
+        additionalEndpoints: [
+            {
+                source: './src/endpoints/edge.ts',
+                destination: `edge`,
+                addRoute: true,
+            },
+        ],
+    },
     optimizeDeps: {
         esbuildOptions: {
             // Node.js global to browser globalThis
