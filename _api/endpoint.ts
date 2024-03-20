92167f94457086b5a9dfde68bd9cfd4aabda1c3a
Luke Hamilton <pies.eth@pm.me>
2024-03-20 00:58:08 -0400

    feat: use base jsonrpc provider

--- _api/endpoint.ts
+++ _api/endpoint.ts
@@ -3,7 +3,7 @@ import { ethers } from 'ethers'
 import ERC20_ABI from '../src/abis/erc20.json'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-    const provider = new ethers.providers.AlchemyProvider('optimism', process.env.VITE_ALCHEMY_KEY)
+    const provider = new ethers.providers.JsonRpcProvider(process.env.VITE_MAINNET_PUBLIC_RPC)
     const address = '0xf467C7d5a4A9C4687fFc7986aC6aD5A4c81E1404'
     const erc20 = new ethers.Contract(address, ERC20_ABI, provider)
     const totalSupply = ethers.utils.formatUnits(await erc20.totalSupply())
