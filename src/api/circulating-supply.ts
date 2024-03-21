10981f66e4a08dc79d0cb6c28484660c4df43740
Luke Hamilton <pies.eth@pm.me>
2024-03-21 11:20:32 -0400

    feat: test numbers

--- src/api/circulating-supply.ts
+++ src/api/circulating-supply.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { ethers } from 'ethers'
+import { ethers, BigNumber } from 'ethers'
 import ERC20_ABI from '../abis/erc20.json'
 import VESTING_ABI from '../abis/vesting.json'
 
@@ -10,18 +10,31 @@ export default async function handler(request: VercelRequest, response: VercelRe
     const incentivesAddress = '0x21a1Ec8c62bbdad4680742B88695F06f55a51bda'
     const flxAlignmentsAddress = '0x638d5CcAF57446363e8Ca5BD09b220EE7A87e8C6'
     const erc20 = new ethers.Contract(address, ERC20_ABI, provider)
-    const totalSupply = ethers.utils.formatUnits(await erc20.totalSupply())
+    // const totalSupply = ethers.utils.formatUnits(await erc20.totalSupply())
+    const totalSupply = await erc20.totalSupply()
+
+    const incentivesBal = await erc20.balanceOf(incentivesAddress)
+    const flxAlignmentBal = await erc20.balanceOf(flxAlignmentsAddress)
+
     const vesting = new ethers.Contract(vestingAddress, VESTING_ABI, provider)
-    let totalLockedTokens = 0
+    let totalLockedVesting
     const vestingSupply = await vesting.totalSupply()
     for (let i = 1; i <= vestingSupply; i++) {
         let plan = await vesting.plans(i)
         if (plan.token.toLowerCase() == address.toLowerCase()) {
-            totalLockedTokens += plan.amount
+            const planAmount = BigNumber.from(plan.amount)
+            totalLockedVesting = vestingSupply.add(planAmount)
         }
     }
-    const vestingTokens = ethers.utils.formatUnits(totalLockedTokens)
+    const circulatingSupply = totalSupply.sub(incentivesBal).sub(flxAlignmentBal).sub(totalLockedVesting)
+    // const vestingTokens = ethers.utils.formatUnits(totalLockedTokens)
 
     response.setHeader('Content-Type', 'text/plain')
-    return response.send({ totalSupply, vestingTokens })
+    return response.send({
+        circulatingSupply: circulatingSupply.toNumber(),
+        totalSupply: totalSupply.toNumber(),
+        totalLockedVesting: totalLockedVesting.toNumber(),
+        incentivesBal: incentivesBal.toNumber(),
+        flxAlignmentBal: flxAlignmentBal.toNumber(),
+    })
 }
--- src/api/total-supply.ts
+++ src/api/total-supply.ts
@@ -1,24 +1,12 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { ethers } from 'ethers'
 import ERC20_ABI from '../abis/erc20.json'
-import VESTING_ABI from '../abis/vesting.json'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const provider = new ethers.providers.JsonRpcProvider(process.env.VITE_MAINNET_PUBLIC_RPC)
     const address = '0xf467C7d5a4A9C4687fFc7986aC6aD5A4c81E1404'
-    const vestingAddress = '0x1bb64AF7FE05fc69c740609267d2AbE3e119Ef82'
     const erc20 = new ethers.Contract(address, ERC20_ABI, provider)
     const totalSupply = ethers.utils.formatUnits(await erc20.totalSupply())
-    const vesting = new ethers.Contract(vestingAddress, VESTING_ABI, provider)
-    let totalLockedTokens = 0
-    const vestingSupply = await vesting.totalSupply()
-    for (let i = 1; i <= vestingSupply; i++) {
-        let plan = await vesting.plans(i)
-        if (plan.token.toLowerCase() == address.toLowerCase()) {
-            totalLockedTokens += plan.amount
-        }
-    }
-    const vestingTokens = ethers.utils.formatUnits(totalLockedTokens)
     response.setHeader('Content-Type', 'text/plain')
-    return response.send({ totalSupply, vestingTokens })
+    return response.send(totalSupply)
 }
