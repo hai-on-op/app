ec28e44430089a03d581ad9c776a444d551343b6
Luke Hamilton <pies.eth@pm.me>
2024-03-21 12:32:40 -0400

    feat: test without destructuring

--- src/api/circulating-supply.ts
+++ src/api/circulating-supply.ts
@@ -3,8 +3,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { kiteTokenStats } from '~/utils/stats'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-    const { circulatingSupply } = await kiteTokenStats()
+    const stats = await kiteTokenStats()
     response.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate=30') // set caching header
     response.setHeader('Content-Type', 'text/plain')
-    return response.send(circulatingSupply)
+    return response.send(stats)
 }
