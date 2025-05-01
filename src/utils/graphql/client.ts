07011cee9e8d90988acd68eeffe44e095efe4809
Luke Hamilton <pies.eth@pm.me>
2025-04-30 22:23:18 -0400

    feat: update subgraph

--- src/utils/graphql/client.ts
+++ src/utils/graphql/client.ts
@@ -13,7 +13,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const uri =
     NETWORK_ID === 10
-        ? 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/hai-mainnet-v2/version/v1.0.0/api'
+        ? 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/hai-mainnet-v2/version/v1.0.1/api'
         : 'https://subgraph.satsuma-prod.com/ea4569e42b10/duuvf2ayrz8e0yxg0udnic--151680/sepolia-staking/api'
 
 export const client = new ApolloClient({
