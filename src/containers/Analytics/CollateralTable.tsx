83988cf2f3fe8184d678c10d204b7273d13b2fad
Drew Fisher <drew@pods.media>
2025-12-12 09:50:31 -0500

    Display next update time as delta with hover for precision

--- src/containers/Analytics/CollateralTable.tsx
+++ src/containers/Analytics/CollateralTable.tsx
@@ -2,14 +2,16 @@ import type { SetState, SortableHeader, Sorting, TokenAnalyticsData } from '~/ty
 import { formatDataNumber } from '~/utils'
 
 import styled from 'styled-components'
-import { Flex, Grid, Text } from '~/styles'
+import { Flex, Grid, Popout, Text } from '~/styles'
 import { TokenArray } from '~/components/TokenArray'
 import { AddressLink } from '~/components/AddressLink'
 import { Table } from '~/components/Table'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 
 type CollateralTableProps = {
     headers: SortableHeader[]
-    rows: TokenAnalyticsData[]
+    rows: (TokenAnalyticsData & {lastUpdateTime?: string, updateDelay?: string})[] 
     sorting: Sorting
     setSorting: SetState<Sorting>
 }
@@ -20,44 +22,123 @@ export function CollateralTable({ headers, rows, sorting, setSorting }: Collater
             headerContainer={TableHeader}
             sorting={sorting}
             setSorting={setSorting}
-            rows={rows.map(({ symbol, delayedOracle, currentPrice, nextPrice, tokenContract, collateralJoin }) => (
-                <Table.Row
-                    key={symbol}
-                    container={TableRow}
-                    headers={headers}
-                    items={[
-                        {
-                            content: (
-                                <Flex $align="center" $gap={8}>
-                                    <TokenArray tokens={[symbol as any]} hideLabel />
-                                    <Text $fontWeight={700}>{symbol}</Text>
-                                </Flex>
-                            ),
-                        },
-                        {
-                            content: tokenContract ? <AddressLink address={tokenContract} /> : <Text>--</Text>,
-                        },
-                        {
-                            content: collateralJoin ? <AddressLink address={collateralJoin} /> : <Text>--</Text>,
-                        },
-                        {
-                            content: <AddressLink address={delayedOracle} />,
-                        },
-                        {
-                            content: <Text>{formatDataNumber(currentPrice?.toString() || '0', 18, 2, true)}</Text>,
-                        },
-                        {
-                            content: <Text>{formatDataNumber(nextPrice?.toString() || '0', 18, 2, true)}</Text>,
-                        },
-                        {
-                            content: <Text>{'Nothing here (yet)'}</Text>,
-                        },
-                    ]}
-                />
-            ))}
+            rows={rows.map(
+                ({
+                    symbol,
+                    delayedOracle,
+                    currentPrice,
+                    nextPrice,
+                    tokenContract,
+                    collateralJoin,
+                    lastUpdateTime,
+                    updateDelay,
+                }) => (
+                    <Table.Row
+                        key={symbol}
+                        container={TableRow}
+                        headers={headers}
+                        items={[
+                            {
+                                content: (
+                                    <Flex $align="center" $gap={8}>
+                                        <TokenArray tokens={[symbol as any]} hideLabel />
+                                        <Text $fontWeight={700}>{symbol}</Text>
+                                    </Flex>
+                                ),
+                            },
+                            {
+                                content: tokenContract ? <AddressLink address={tokenContract} /> : <Text>--</Text>,
+                            },
+                            {
+                                content: collateralJoin ? <AddressLink address={collateralJoin} /> : <Text>--</Text>,
+                            },
+                            {
+                                content: <AddressLink address={delayedOracle} />,
+                            },
+                            {
+                                content: <Text>{formatDataNumber(currentPrice?.toString() || '0', 18, 2, true)}</Text>,
+                            },
+                            {
+                                content: <Text>{formatDataNumber(nextPrice?.toString() || '0', 18, 2, true)}</Text>,
+                            },
+                            {
+                                content:
+                                    lastUpdateTime !== undefined && updateDelay !== undefined ? (
+                                        <TimeLeft
+                                            timestamp={(Number(lastUpdateTime) + Number(updateDelay)) * 1000}
+                                        />
+                                    ) : (
+                                        <Text>Loading...</Text>
+                                    ),
+                            },
+                        ]}
+                    />
+                )
+            )}
         />
     )
 }
+
+function TimeLeft({ timestamp }: { timestamp: number }) {
+    const [hovered, setHovered] = useState(false)
+    const rtf = new Intl.RelativeTimeFormat('en-US', { numeric: 'auto', style: 'narrow' })
+    const date = new Date(timestamp)
+
+    const { data } = useQuery({
+        queryKey: ['timeleft', timestamp],
+        queryFn: () => {
+            const diffMs = timestamp - Date.now()
+            const diffSec = Math.round(diffMs / 1000)
+
+            let value: number
+            let unit: Intl.RelativeTimeFormatUnit
+
+            if (Math.abs(diffSec) < 60) {
+                value = diffSec
+                unit = 'second'
+            } else if (Math.abs(diffSec) < 3600) {
+                value = Math.round(diffSec / 60)
+                unit = 'minute'
+            } else if (Math.abs(diffSec) < 86400) {
+                value = Math.round(diffSec / 3600)
+                unit = 'hour'
+            } else {
+                value = Math.round(diffSec / 86400)
+                unit = 'day'
+            }
+
+            return rtf.format(value, unit)
+        },
+        refetchInterval: 1000,
+        staleTime: 0,
+    })
+
+    if (!data) return null
+
+    return (
+        <TimeContainer $align="center" $gap={2} $grow={0}>
+            <Text onPointerEnter={() => setHovered(true)} onPointerLeave={() => setHovered(false)}>
+                {data}
+            </Text>
+            <Popout
+                hidden={!hovered}
+                $anchor="bottom"
+                $textAlign="center"
+                $float="right"
+                $width="fit-content"
+                $padding="4px"
+                $margin="20px"
+            >
+                at {date.getHours()}:{date.getMinutes().toString().padStart(2, '0')}:
+                {date.getSeconds().toString().padStart(2, '0')}
+            </Popout>
+        </TimeContainer>
+    )
+}
+
+const TimeContainer = styled(Flex)`
+    position: relative;
+`
 
 const TableHeader = styled(Grid)`
     grid-template-columns: repeat(7, 1fr);
